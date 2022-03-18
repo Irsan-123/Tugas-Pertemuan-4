1d49b3d2b5731147c7941c611fc6abab45fe5568
Irsan-123 <72791758+Irsan-123@users.noreply.github.com>
2022-03-18 14:42:50 +0700

    Add files via upload

---
+++ src/component/Main.js
@@ -0,0 +1,15 @@
+import Hello from "./Hello";
+
+const Main = () => {
+  return (
+    <main>
+      <Hello name="Muhammad Irsan Khalish" />
+      <Hello name="Jonah" />
+      <Hello name="Jon Snow" />
+      <Hello name="Aris" />
+      <Hello name="Lidya" />
+    </main>
+  );
+};
+
+export default Main;
